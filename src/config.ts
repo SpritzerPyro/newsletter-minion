2ef5c670c2955f8f3cea1f1ece0fd7d361025f71
pueri <pyro@outlook.at>
2020-09-14 16:48:25 +0200

    Add outDir property to config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,13 @@
 import convict from 'convict';
 
 export const config = convict({
+  outDir: {
+    arg: 'out-dir',
+    default: 'out',
+    doc: 'Output directory of the created HTML files.',
+    env: 'NEWSLETTER_MINION_OUT_DIR',
+    format: 'String',
+  },
   srcDir: {
     arg: 'src-dir',
     default: 'src',
